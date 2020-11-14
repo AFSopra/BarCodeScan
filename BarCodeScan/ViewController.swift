5e70da9de653759c760506c3244ae13f3acc7ddc
AFSopra <alvaro.ferrandez.sopra@gmail.com>
2020-11-14 11:56:55 +0100

    Initial Commit

---
+++ BarCodeScan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarCodeScan
+//
+//  Created by sopra on 14/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
